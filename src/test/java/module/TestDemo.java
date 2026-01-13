7427999775e73ec5fe5192a1be8fc2db45131444
Administrator <Administrator@WINDOWS-OPOQ39G>
2026-01-13 16:10:11 +0530

    Harry commited Ron Commited Ginee Commited

---
+++ src/test/java/module/TestDemo.java
@@ -0,0 +1,15 @@
+package module;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	@Test(groups="smoke")
+	public void HPTest()
+	{
+		System.out.println("Harry");
+		System.out.println("Ron");
+		System.out.println("ginee");
+	}
+
+
+}
---
+++ src/test/java/module/TestDemo1.java
@@ -0,0 +1,14 @@
+package module;
+
+import org.testng.annotations.Test;
+
+public class TestDemo1 {
+	@Test(groups="regression")
+	public void HP1Test()
+	{
+		System.out.println("Hermonie");
+		System.out.println("Hagrid");
+	}
+
+
+}
